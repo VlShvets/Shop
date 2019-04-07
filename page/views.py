374d1b7416bf40551d95aaf542d8dfc8cee20b46
Vladimir Shvets <vladimir-shvets@list.ru>
2019-04-07 20:52:33 +0300

    Was added a few htmls.

--- page/views.py
+++ page/views.py
@@ -6,55 +6,30 @@ from django.http import Http404
 
 # Create your views here.
 def index(request):
-    cats = Category.objects.all()
-    s = "Категории: " + "<br><br>"
-    for cat in cats:
-        s = s + cat.name + "<br><br>"
-       
-    return HttpResponse(s)
+    cats = Category.objects.all().order_by("name")
+           
+    return render(request, "index.html", { "cats": cats })
 
 def category(request, cat_id):
-    if cat_id == None:
-        cat = Category.objects.first()
-    else:
-        try:
-            cat = Category.objects.get(pk = cat_id)
-        except:
-            raise Http404
+    cats = Category.objects.all().order_by("name")
+    try:
+        cat = Category.objects.get(pk = cat_id)
+    except:
+        raise Http404
 
     goods = Good.objects.filter(category = cat).order_by("name")
-    s = "Категория: " + cat.name + "<br><br>"
-    
-    for good in goods:
-        s = s + "(" + str(good.pk) + ") " + good.name + "<br>"
         
-    return HttpResponse(s)
+    return render(request, "index.html", { "category": cat, "cats": cats, "goods": goods})
    
 def goods(request):
-    goods = Good.objects.all()
-    s = "Товары:" + "<br><br>"
-    for good in goods:
-        s = s + good.name + "<br>" + good.category.name + "<br>" + good.description + "<br>"
-    
-        if not good.in_stock:
-            s = s + "Нет в наличии!"
-            
-        s = s + "<br><br>"
+    goods = Good.objects.all().order_by("name")
 
-    return HttpResponse(s)
+    return render(request, "index.html", { "goods": goods})
    
 def good(request, good_id):
-    if good_id == None:
-        good = Good.objects.first()
-    else:
-        try:
-            good = Good.objects.get(pk = good_id)
-        except Good.DoesNotExist:
-            raise Http404
+    try:
+        good = Good.objects.get(pk = good_id)
+    except Good.DoesNotExist:
+        raise Http404
 
-    s = good.name + "<br><br>" + good.category.name + "<br><br>" + good.description
-    
-    if not good.in_stock:
-        s = s + "<br><br>" + "Нет в наличии!"
-
-    return HttpResponse(s)
+    return render(request, "good.html", { "good": good})
