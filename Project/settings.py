ac3dac247b73b0d8f2c6155b0e743f794b3f5389
Vladimir Shvets <vladimir-shvets@list.ru>
2019-04-08 00:09:15 +0300

    Was changed htmls.

--- Project/settings.py
+++ Project/settings.py
@@ -119,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
--- page/views.py
+++ page/views.py
@@ -22,14 +22,16 @@ def category(request, cat_id):
     return render(request, "index.html", { "category": cat, "cats": cats, "goods": goods})
    
 def goods(request):
+    cats = Category.objects.all().order_by("name")
     goods = Good.objects.all().order_by("name")
 
-    return render(request, "index.html", { "goods": goods})
+    return render(request, "index.html", { "cats": cats, "goods": goods})
    
 def good(request, good_id):
+    cats = Category.objects.all().order_by("name")
     try:
         good = Good.objects.get(pk = good_id)
     except Good.DoesNotExist:
         raise Http404
 
-    return render(request, "good.html", { "good": good})
+    return render(request, "good.html", { "cats": cats, "good": good})
