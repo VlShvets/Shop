538eb31addc466456da3021ef71448c14c2c8cfa
Vladimir Shvets <vladimir-shvets@list.ru>
2019-04-08 09:25:00 +0300

    Was add paginator. There are some errors.

--- page/views.py
+++ page/views.py
@@ -1,37 +1,44 @@
 from django.shortcuts import render
 
 from page.models import Category, Good
-from django.http import HttpResponse
+from django.core.paginator import Paginator, InvalidPage
 from django.http import Http404
 
+
 # Create your views here.
-def index(request):
-    cats = Category.objects.all().order_by("name")
-           
-    return render(request, "index.html", { "cats": cats })
-
-def category(request, cat_id):
-    cats = Category.objects.all().order_by("name")
+def index(request, cat_id):
     try:
-        cat = Category.objects.get(pk = cat_id)
-    except:
-        raise Http404
-
-    goods = Good.objects.filter(category = cat).order_by("name")
+        page_num = request.GET["page"]
+    except KeyError:
+        page_num = 1
+        
+    cats = Category.objects.all().order_by("name")
+     
+    if cat_id == None:
+        cat = Category.objects.first()
+    else:
+        cat = Category.object.get(pk = cat_id)
+        
+    paginator = Paginator(Good.object.filter(category = cat).order_by("name"), 10)
+    
+    try:
+        goods = paginator.page(page_num)
+    except InvalidPage:
+        goods = paginator.page(1)
         
     return render(request, "index.html", { "category": cat, "cats": cats, "goods": goods})
    
-def goods(request):
-    cats = Category.objects.all().order_by("name")
-    goods = Good.objects.all().order_by("name")
-
-    return render(request, "index.html", { "cats": cats, "goods": goods})
-   
 def good(request, good_id):
+    try:
+        page_num = request.GET["page"]
+    except KeyError:
+        page_num = 1
+        
     cats = Category.objects.all().order_by("name")
+    
     try:
         good = Good.objects.get(pk = good_id)
     except Good.DoesNotExist:
-        raise Http404
+        raise Http404;
 
-    return render(request, "good.html", { "cats": cats, "good": good})
+    return render(request, "good.html", { "cats": cats, "good": good, "pn": page_number})
